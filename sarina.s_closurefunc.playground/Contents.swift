61707abe7cd55f7106db72e53ceb41db5316f62a
sarinashepard <67968973+sarinashepard@users.noreply.github.com>
2020-08-04 17:46:40 -0500

    Add files via upload

---
+++ sarina.s_closurefunc.playground/Contents.swift
@@ -0,0 +1,11 @@
+import UIKit
+//function
+let multiply: (Int, Int) -> Int  = { (num1, num2) -> Int in
+return num1 * num2
+}
+//closure
+multiply(12, 3) // will return 36
+print(multiply(12,3))
+
+
+
